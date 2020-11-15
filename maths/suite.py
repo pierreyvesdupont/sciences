fbc623de37a1a1738194cd3646289552e6358f9e
pierreyvesdupont <dupontpierreyves@gmail.com>
2020-11-16 00:19:36 +0100

    add graphical generation

--- maths/suite.py
+++ maths/suite.py
@@ -1,6 +1,10 @@
 from math import *
 import random
 
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
 def all_equal(iterator):
     return len(set(iterator)) <= 1
 
@@ -23,8 +27,10 @@ class Suite:
         self.u_zero = None
         self.first_value = None
 
-        if len(args) == 2:
-            self.init_with_expression(args[0], args[1])
+        if len(args) == 1:
+            self.init_generation_implicite(args[0])
+        elif len(args) == 2:
+            self.init_generation_recursive(args[0], args[1])
         elif len(args) == 3:
             self.init_with_terms(args[0], args[1], args[2])
 
@@ -43,35 +49,39 @@ class Suite:
         if type == Suite.TYPE_ARITHMETICAL:
             raison=(u_b.value - u_a.value) / (u_b.index - u_a.index)
             self.expression_un = "{} + {}{}{}".format(Suite.U_N, ("", "(")[raison < 0], raison, ("", ")")[raison < 0])
-            self.u_zero = u_a.value - (u_a.index + 1) * raison
+            self.u_zero = u_a.value - (u_a.index) * raison
         elif type == Suite.TYPE_GEOMETRICAL:
             power = float(1) / float(u_b.index - u_a.index)
             raison=(float(u_b.value) / float(u_a.value))**(power)
             print("power:[{}] raison:[{}] u_a.index:[{}] u_a.value:[{}]".format(power, raison, u_a.index, u_a.value))
             self.expression_un = "{} * {}{}{}".format(Suite.U_N, ("", "(")[raison < 0], raison, ("", ")")[raison < 0])
-            self.u_zero = float(u_a.value) / (raison**(u_a.index+1))
+            self.u_zero = float(u_a.value) / (raison**(u_a.index))
 
-    def init_with_expression(self, expression, first_value):
+    def init_generation_implicite(self, expression):
         self.expression_un = None
         self.expression_n = None
         self.u_zero = None
         self.first_value = None
 
-        if self.isExpressionRecursive(expression):
-            # génération récursive
-            print("génération récursive")
-            self.expression_un = expression
-            self.u_zero = first_value
-        else:
-            print("génération implicite")
-            self.expression_n = expression
-            self.n_zero = first_value
-            if self.isArithmetical():
-                self.u_zero = self.term(0)
-                self.expression_un = "{} + {}".format(Suite.U_N, self.term(1) - self.u_zero)
-            elif self.isGeometrical():
-                self.u_zero = self.term(0)
-                self.expression_un = "{} * {}".format(Suite.U_N, self.term(1) / self.u_zero)
+        print("génération implicite")
+        self.expression_n = expression
+        self.n_zero = None
+        if self.isArithmetical():
+            self.u_zero = self.term(0)
+            self.expression_un = "{} + {}".format(Suite.U_N, self.term(1) - self.u_zero)
+        elif self.isGeometrical():
+            self.u_zero = self.term(0)
+            self.expression_un = "{} * {}".format(Suite.U_N, self.term(1) / self.u_zero)
+
+    def init_generation_recursive(self, expression, first_value):
+        self.expression_un = None
+        self.expression_n = None
+        self.u_zero = None
+        self.first_value = None
+
+        print("génération récursive")
+        self.expression_un = expression
+        self.u_zero = first_value
 
     def expression (self):
         return (self.expression_un, self.expression_n)[self.expression_un == None]
@@ -84,7 +94,7 @@ class Suite:
         if self.expression_un == None:
             result = eval(self.expression_n, Suite.GLOBALS, {Suite.N: term_index})
         else:
-            result = eval(self.expression_un, Suite.GLOBALS, {Suite.U_N: self.u_zero})
+            result = self.u_zero
             if term_index > 0:
                 result = eval(self.expression_un, Suite.GLOBALS, {Suite.U_N: self.term(term_index - 1)})
 
@@ -113,6 +123,21 @@ class Suite:
 
         return all_equal(results)
 
+def draw_suite(suite):
+    x = np.arange(0, 2,0.01)
+    fig, ax = plt.subplots()
+
+    data = [0] * 10
+    for index in range(10):
+        data[index] = suite.term(index)
+    ax.plot(data)
+
+    ax.set(xlabel='abscisses', ylabel='ordonnées',
+        title='Suite')
+    ax.grid()
+
+    plt.show()
+
 def study_suite(suite):
     isArithemical = suite.isArithmetical()
     if isArithemical:
@@ -121,15 +146,22 @@ def study_suite(suite):
         isGeometrical = suite.isGeometrical()
         if isGeometrical:
             print("{} is geometic.".format(suite.expression()))
+        else:
+            print("{} is neither arithemic nor geometic.".format(suite.expression()))
 
     for index in range(8):
         print("u{} = {}".format(index, suite.term(index)))
 
+    draw_suite(suite)
+
 #study_suite(Suite(Suite.TYPE_ARITHMETICAL, SuiteTerm(3, 6), SuiteTerm(5, -2)))
-study_suite(Suite(Suite.TYPE_GEOMETRICAL, SuiteTerm(3, 6), SuiteTerm(5, 2)))
-#study_suite(Suite("2*n + 1", 1))
+#study_suite(Suite(Suite.TYPE_GEOMETRICAL, SuiteTerm(3, 6), SuiteTerm(5, 2)))
+#study_suite(Suite("2*n + 1"))
 #study_suite(Suite("u_n + 1", 9))
-#study_suite(Suite("u_n * 0.6", 1))
-#study_suite(Suite("4 * 5**n", 1))
+study_suite(Suite("u_n * 0.6", 1))
+#study_suite(Suite("4 * 5**n"))
 
 #study_suite(Suite("(u_n + 2)**2 * 0.6", 1))
+#study_suite(Suite("(n + 2)**2 * 0.6"))
+#study_suite(Suite("(-n+2)**3 * 0.6"))
+#study_suite(Suite("(-n+2)**3 +1"))
